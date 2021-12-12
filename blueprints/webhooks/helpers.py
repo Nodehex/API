038e4cfb74560e2fae1d8595f49368e8111f5ceb
Sebastian Olsen <30187002+Spacebjorn@users.noreply.github.com>
2021-12-12 10:43:47 +0100

    Feature/update api (#4)
    * removed github pip requirement
    
    * rewrote the helper function to no longer use the random python module

--- blueprints/webhooks/helpers.py
+++ blueprints/webhooks/helpers.py
@@ -1,17 +1,24 @@
 import hmac
+import subprocess
+import os
 from flask import request, current_app
-from git import Repo
+from contextlib import contextmanager
+
+def change_directory(new_directory):
+    old_directory = os.getcwd()
+    os.chdir(new_directory)
+    try:
+        yield
+    finally:
+        os.chdir(old_directory)
 
 def handle_repository_update(pullrequest, config_name):
   if pullrequest['action'] == 'closed' and pullrequest['pull_request']['merged']:
-      repo = Repo(current_app.config.get(config_name)) 
-      origin = repo.remotes.origin 
-      origin.pull()
-      print('Repository updated with pull request {}'.format(pullrequest['pull_request']['head']['label']))
-
+      with change_directory(current_app.config.get(config_name)):
+          subprocess.run("git pull")
 
 def validate_secret(req, config_name):
-    signature = req.headers.get('X-Hub-Signature') 
+    signature = req.headers.get('X-Hub-Signature')
     sha, signature = signature.split('=')
 
     secret = str.encode(current_app.config.get(config_name))
